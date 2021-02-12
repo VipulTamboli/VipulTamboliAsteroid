65444c9177bea6a101d250d9815b3286531bba9e
Vipul Tamboli <68987811+VipulTamboli@users.noreply.github.com>
2021-02-12 10:00:32 +0530

    Create swpaner.cs

---
+++ swpaner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swpaner : MonoBehaviour
+{
+       public GameObject[] asteroids;
+       public GameObject[] spawnPositions;
+       public float spawnRate;
+       private float NextSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NextSpawn>0)
+        NextSpawn-=Time.deltaTime;
+        if (NextSpawn<=0)
+        Spawn();
+    }
+      private void Spawn()
+      {
+        NextSpawn = spawnRate;
+        Vector2 position = spawnPositions[Random.Range(0,spawnPositions.Length)].transform.position;
+        GameObject asteroidClone = Instantiate(asteroids[Random.Range(0,asteroids.Length)],new Vector2(position.x,position.y),transform.rotation);
+        asteroidClone.SetActive(true);
+       }
+}
